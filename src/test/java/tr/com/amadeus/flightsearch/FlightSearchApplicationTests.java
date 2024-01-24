ec744ecc8a18240dc7563c9019a3be8c8604afb1
Berkay Yalcin <kemalberkayyalcin@gmail.com>
2024-01-24 16:59:05 +0300

    Base Project Structure

---
+++ src/test/java/tr/com/amadeus/flightsearch/FlightSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.amadeus.flightsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
